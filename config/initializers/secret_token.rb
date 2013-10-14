8d38b439315cdbec1a208c36820edef99ee0f9f7
Sumanth Ravipati <ravipati.sumanth@gmail.com>
2013-10-13 21:19:10 -0400

    Added secret token file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sumoexpense::Application.config.secret_key_base = '2f220edb176f9cc69a36c3951f919ebd49a42032aee5d950e256bbd3133a74ac0b08dff1b44969438889189a6880c8d402f56027833c832c57ccbd161ee54c13'
